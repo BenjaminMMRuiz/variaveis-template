f4555a77e538d1a92ed8b015c23420298a08f7a9
BenjaminMMRuiz <benjaminmartinhomoyaruiz@gmail.com>
2023-05-03 08:58:59 +0100

    Exercicios de Variaveis

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+//let nome = prompt ("digite seu nome:")
 
+//let idade = Number(prompt("Digite sua idade:"))
+
+//idade = Number(idade)
+
+//let presenca = confirm("Confirme sua presença:")
+
+//console.log("Oi, Turma!")
+//console.log(typeof nome)
+//console.log(typeof idade)
+//console.log(typeof presenca)
+
+//let nomeDoCachorro = prompt (" digite o nome do seu cachorro?")
+//console.log(nomeDoCachorro)
+
+//console.log("Estudante", nome, "tem", idade,"anos de presença", presenca)
+
+//console.log("nome:", typeof nome, ",idade:", typeof idade, ",presença" ,typeof presenca)
+
+const dataDeNacimento = prompt("Digite sua data de nascimento:" )
+
+let nome = prompt("Digite seu nome completo:" )
+
+let humano = confirm("Você é um ser humano?" )
+
+console.log("Nome completo", nome, "Data de ñacimento", dataDeNacimento ,"Você é humano", humano)
+
+console.log("Nome completo:", typeof nome, ",Data de Nascimento:", typeof dataDeNacimento, ",Humano?" ,typeof humano)
